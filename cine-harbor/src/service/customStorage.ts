6c7a3f5c241a6b9302fe955761841d9201207575
Milena <milena.alcantara@irede.org.br>
2024-05-07 11:18:31 -0300

    adapt localStorage request

--- cine-harbor/src/service/customStorage.ts
+++ cine-harbor/src/service/customStorage.ts
@@ -1,53 +1,56 @@
-class Storage {
-  #createFavoriteList() {
-    localStorage.setItem("favoriteList", JSON.stringify([]));
+function createFavoriteList() {
+  localStorage.setItem("favoriteList", JSON.stringify([]));
+}
+
+function getFavoriteList(): string[] {
+  const list = localStorage.getItem("favoriteList");
+  console.log("type list:", typeof list);
+  let favoriteList = list ? JSON.parse(list) : [];
+
+  if(favoriteList == null) {
+    createFavoriteList();
+    favoriteList = JSON.parse(list as any);
   }
 
-  // Create
-  addItem(item) {
-    let list = this.getFavoriteList();
+  return favoriteList;
+}
+
+function hasItemOnFavoriteList(id: string): boolean {
+  const list = getFavoriteList();
+  let isOnTheList = false;
+
+  list.forEach((element) => {
+    if (element == id) {
+      isOnTheList = true;
+    }
+  });
+  
+  return isOnTheList;
+}
+
+function addItemToFavoritList(item: string) {
+  let list = getFavoriteList();
+
+  if (item) {
     list.push(item);
     localStorage.setItem("favoriteList", JSON.stringify(list));
   }
-
-  // Read
-  getItem(id) {
-    const list = this.getFavoriteList();
-    let isOnTheList = false;
-
-    list.forEach((element) => {
-      if (element.id == id) {
-        isOnTheList = true;
-      }
-    });
-    // return item
-    return isOnTheList;
-  }
-
-  // Delete
-  removeItem(id) {
-    let list = this.getFavoriteList();
-
-    list.forEach((element, index) => {
-      if (element.id == id) {
-        list.splice(index, 1);
-      }
-    });
-    localStorage.setItem("favoriteList", JSON.stringify(list));
-  }
-
-  getFavoriteList() {
-    let favoriteList = JSON.parse(localStorage.getItem("favoriteList"));
-
-    if (favoriteList == null) {
-      this.#createFavoriteList();
-      favoriteList = JSON.parse(localStorage.getItem("favoriteList"));
-    }
-
-    return favoriteList;
-  }
 }
 
-const storage = new Storage();
+function removeItem(id: string) {
+  let list = getFavoriteList();
 
-export default storage;
+  list.forEach((element, index) => {
+    if (element == id) {
+      list.splice(index, 1);
+    }
+  });
+  localStorage.setItem("favoriteList", JSON.stringify(list));
+}
+
+export {
+  getFavoriteList,
+  hasItemOnFavoriteList,
+  addItemToFavoritList,
+  removeItem
+};
